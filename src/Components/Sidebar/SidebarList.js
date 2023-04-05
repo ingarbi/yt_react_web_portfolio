3d36c2f13cf97a5cf7b6940a7048f2cb777c69a3
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-05 17:24:00 +0300

    #5 list items for sidebar

--- src/Components/Sidebar/SidebarList.js
+++ src/Components/Sidebar/SidebarList.js
@@ -1,25 +1,86 @@
-import React from 'react'
-import "./SidebarList.css"
-import profilepicture from "../../Image/profile.jpg"
+import React from "react";
+import "./SidebarList.css";
+import profilepicture from "../../Image/profile.jpg";
+import {
+  FcHome,
+  FcNightPortrait,
+  FcTodoList,
+  FcContacts,
+  FcFactory,
+  FcSalesPerformance,
+} from "react-icons/fc";
+import { MdBiotech } from "react-icons/md";
+import { MdCastForEducation } from "react-icons/md";
 
-const SidebarList = ({expandSidebar}) => {
+const SidebarList = ({ expandSidebar }) => {
   return (
     <React.Fragment>
+      {expandSidebar ? (
+        <div className="navbar-items">
+          <div className="sidebar-profile-pic">
+            <img src={profilepicture} alt="" />
+          </div>
 
-        {expandSidebar ? (
-        <div className='navbar-items'>
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} /> Home
+            </li>
+            <li className="nav-item">
+              <FcNightPortrait size={25} /> About
+            </li>
+            <li className="nav-item">
+              <FcFactory size={25} /> Work Experience
+            </li>
+            <li className="nav-item">
+              <MdBiotech color="orange" size={25} /> Tech Stack
+            </li>
+            <li className="nav-item">
+              <MdCastForEducation size={25} color="yellow"/> Education
+            </li>
+            <li className="nav-item">
+              <FcTodoList size={25} /> Projects
+            </li>
+            <li className="nav-item">
+              <FcSalesPerformance size={25} /> Testimonial
+            </li>
+            <li className="nav-item">
+              <FcContacts size={25} /> Contacts
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className="navbar-items">
+                    <ul>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcNightPortrait size={25} />
+            </li>
+            <li className="nav-item">
+              <FcFactory size={25} />
+            </li>
+            <li className="nav-item">
+              <MdBiotech color="orange" size={25} />
+            </li>
+            <li className="nav-item">
+              <MdCastForEducation size={25} color="yellow" />
+            </li>
+            <li className="nav-item">
+              <FcTodoList size={25} />
+            </li>
+            <li className="nav-item">
+              <FcSalesPerformance size={25} />
+            </li>
+            <li className="nav-item">
+              <FcContacts size={25} />
+            </li>
+          </ul>
 
-            <div className='sidebar-profile-pic'>
-                <img src={profilepicture} alt="" />
-            </div>
-
-        </div>) : (<div>
-            
-        </div>)}
-
-
+        </div>
+      )}
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default SidebarList
+export default SidebarList;
