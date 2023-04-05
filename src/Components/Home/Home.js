8316f9122713f3003b94710c38f063f31c61626b
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-05 21:44:22 +0300

    #6 typewriter

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./Home.css"
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
-    <div>Home section</div>
+    <div className='container-fluid home'>
+      <div className='container home-content'>
+        <h1>Hi I'm</h1>
+        <h3>
+        <Typewriter
+          options={{
+            strings: ['Python Developer', 'Full Stack Developer', 'Web Developer', 'Just Programmer'],
+            autoStart: true,
+            loop: true,
+            delay: 100
+          }}
+        />
+        </h3>
+      </div>
+    </div>
   )
 }
 
